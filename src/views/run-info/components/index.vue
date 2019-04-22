c063fc8cb8f4de6df0ad1bc0127df93281a553a6
识迩 <tianbin@protonmail.ch>
2019-04-22 11:30:49 +0800

    优化报表信息展示

--- src/views/run-info/components/index.vue
+++ src/views/run-info/components/index.vue
@@ -18,19 +18,27 @@
         </div>
         <div class="rightDiv">
           <div style="margin: 30px 30px 30px 15px">
-           <div
-            class="chart"
-            id="lineChart"
-            :style="{height:mapHeight/8*2.5+'px',float:'left'}"
-           ></div>
-           <div
-            class="chart"
-            id="barChart"
-            :style="{height:mapHeight/8*2.5+'px',float:'right'}"
-
-           ></div>
+            <div style="width: 50%;float: left">
+              <span style="font-size: large;font-weight: bold">{{this.getDeviceFocusMap.title}}实时曲线</span>
+            </div>
+            <div style="width: 50%;float: right">
+              <span style="font-size: large;font-weight: bold">报警统计</span>
+            </div>
+           <div>
+              <div
+               class="chart"
+               id="lineChart"
+               :style="{height:mapHeight/8*2.5+'px',float:'left'}"
+              ></div>
+               <div
+                 class="chart"
+                 id="barChart"
+                 :style="{height:mapHeight/8*2.5+'px',float:'right'}"
+               >
+               </div>
+           </div>
           </div>
-           <div v-show="flag" class="runInfo" :style="{height:mapHeight/8*4+'px'}">
+           <div v-show="flag" class="runInfo" :style="{height:mapHeight/8*3.5+'px'}">
             <runinfo-show class="runInfoShow"></runinfo-show>
            </div>
 
@@ -48,6 +56,8 @@
     import {productDataOnMap} from "@/api/product"
     import runinfo from "@/views/run-info/controller-run-info";
     import { config } from "@/config/index";
+    import {getDeviceByByteDataAndType,getCmdMapByDevice} from "@/dataparse/model/deviceAdapter";
+    import {getControllerByteData,getControllerType} from '@/api/controller'
     export default {
      name: "map-component",
      components: {
@@ -82,7 +92,17 @@
         reporttime: "" //数据发布时间
        },
        src: "//i.tianqi.com/index.php?c=code&id=19&icon=1&temp=1&num=5&site=12",
-       weatherSrc: ""
+       weatherSrc: "",
+       chartDeviceInfo: {
+        chartName: "",
+       },
+       chartNameArray: [],
+       getDeviceFocusMap: {
+        title: "",
+        value: '',
+        unit: ""
+       },
+       mockInfoMap:{}
       };
      },
      mounted () {
@@ -91,260 +111,304 @@
       map.centerAndZoom(point, 6);
       map.enableScrollWheelZoom(true);
       map.enableDoubleClickZoom(true);
-      this.loadMapData(map);
-      this.drawLine();
+      this.loadMapData(map)
      },
      methods: {
-      getlocation () {
-       //定位方法
-       var map = new BMap.Map("weather");
-       var point = new BMap.Point(116.331398, 39.897445);
-       map.centerAndZoom(point, 12);
-       var geolocation = new BMap.Geolocation();
-       geolocation.getCurrentPosition(function (r) {
-        if (this.getStatus() == BMAP_STATUS_SUCCESS) {
-         var mk = new BMap.Marker(r.point);
-         map.addOverlay(mk);
-         map.panTo(r.point);
-         alert("您的位置：" + r.point.lng + "," + r.point.lat);
-        } else {
-         alert("failed" + this.getStatus());
-        }
-       });
-      },
-      loadMapData (map) {
-       //加载地图
-       if (checkPermission(['3', '5'])) this.userId = this.$store.state.user.userId;
-       productDataOnMap({userId: this.userId}).then((res) => {
-        this.showMapData(map, res.data.data)
-       })
-      },
-      /**
-       * 显示地图标点的设备相关信息
-       */
-      showMapData (map, data) {
-       this.mapPoints = data;
-       let markers = [];
-       for (let i = 0; i < this.mapPoints.length; i++) {
-        if (
-         this.mapPoints[i].longitude == null ||
-         this.mapPoints[i].latitude == null
-        ) {
-         continue;
-        }
-        let points = new BMap.Point(
-         this.mapPoints[i].longitude,
-         this.mapPoints[i].latitude
-        );
-        let mk = new BMap.Marker(points);
-        markers.push(mk);
-        let address = this.mapPoints[i].province + this.mapPoints[i].city + this.mapPoints[i].district
-        mk.addEventListener("click", () => {
-         this.weatherSrc = this.src;
-         this.flag = true;
-         let cityPinyin = ConvertPinyin(
-          this.getArea(address).City
-         );
-         switch (cityPinyin) {
-          case "beijing":
-          case "taian":
-           cityPinyin = cityPinyin + "1";
-           break;
-          default:
-           break;
+       getlocation () {
+         //定位方法
+         var map = new BMap.Map("weather");
+         var point = new BMap.Point(116.331398, 39.897445);
+         map.centerAndZoom(point, 12);
+         var geolocation = new BMap.Geolocation();
+         geolocation.getCurrentPosition(function (r) {
+           if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+             var mk = new BMap.Marker(r.point);
+             map.addOverlay(mk);
+             map.panTo(r.point);
+             alert("您的位置：" + r.point.lng + "," + r.point.lat);
+           } else {
+             alert("failed" + this.getStatus());
+           }
+         });
+       },
+       loadMapData (map) {
+         //加载地图
+         if (checkPermission(['3', '5'])) this.userId = this.$store.state.user.userId;
+         productDataOnMap({userId: this.userId}).then((res) => {
+           this.showMapData(map, res.data.data)
+         })
+       },
+       /**
+        * 显示地图标点的设备相关信息
+        */
+       showMapData (map, data) {
+         this.mapPoints = data;
+         let markers = [];
+         for (let i = 0; i < this.mapPoints.length; i++) {
+           if (
+             this.mapPoints[i].longitude == null ||
+             this.mapPoints[i].latitude == null
+           ) {
+             continue;
+           }
+           let points = new BMap.Point(
+             this.mapPoints[i].longitude,
+             this.mapPoints[i].latitude
+           );
+           let mk = new BMap.Marker(points);
+           markers.push(mk);
+           let address = this.mapPoints[i].province + this.mapPoints[i].city + this.mapPoints[i].district
+           mk.addEventListener("click", () => {
+             this.weatherSrc = this.src;
+             this.flag = true;
+             let cityPinyin = ConvertPinyin(
+               this.getArea(address).City
+             );
+             switch (cityPinyin) {
+               case "beijing":
+               case "taian":
+                 cityPinyin = cityPinyin + "1";
+                 break;
+               default:
+                 break;
+             }
+             this.weatherSrc = this.src + "&py=" + cityPinyin;
+             this.$store.state.user.deviceRunInfoNo = this.mapPoints[i].controllerNo;
+             this.getLineChartDeviceFocus(this.$store.state.user.deviceRunInfoNo)
+           });
          }
-         this.weatherSrc = this.src + "&py=" + cityPinyin;
-         this.$store.state.user.deviceRunInfoNo = this.mapPoints[i].controllerNo;
-        });
+         new BMapLib.MarkerClusterer(map, {markers: markers});
+         this.drawLine()
+       },
+       /**
+        * 绘制报表
+        */
+       drawLine () {
+         //绘制报表
+         let that = this;
+         console.log(that)
+         var dom = document.getElementById("lineChart");
+         let dom2 = document.getElementById("barChart");
+         var myChart = this.$echarts.init(dom);
+         let myChart2 = this.$echarts.init(dom2);
+         let optionLine = null;
+         let optionBar = null;
+
+         function randomData () {
+           now = new Date(+now + oneDay);
+           //value = Math.round(Math.random() * 20 + 120 + 1);
+           value = that.getDeviceFocusMap.value
+           return {
+             name: now.toString(),
+             value: [
+               [now.getTime()],
+               //Math.round(value)
+               value
+             ]
+           };
+         }
+
+         var data = [];
+         var now = new Date(new Date().getTime() - 2 * 60 * 1000)
+         var oneDay = 1 * 1000;
+         var value = this.getDeviceFocusMap.value;
+         for (var i = 0; i < 100; i++) {
+           data.push(randomData());
+         }
+         var title = that.getDeviceFocusMap.title
+         optionLine = {
+           title: {},
+           tooltip: {
+             trigger: "axis",
+             formatter: function (params) {
+               params = params[0];
+               var date = new Date(params.name);
+               return (
+                 date.getHours() + ":" +
+                 ((date.getMinutes() > 9) ? date.getMinutes() : "0" + date.getMinutes()) +
+                 "=>" +
+                 params.value[1] +
+                 (params.value[1] ? that.getDeviceFocusMap.unit : "无数据")
+               );
+             },
+             axisPointer: {
+               animation: false
+             }
+           },
+           xAxis: {
+             type: "time",
+             splitLine: {
+               show: false
+             }
+           },
+           yAxis: {
+             type: "value",
+             boundaryGap: [0, "100%"],
+             splitLine: {
+               show: false
+             },
+
+           },
+           grid: {
+             y: '10%',
+             x2: '10%',
+             y2: '10%',
+             x: '10%'
+           },
+           series: [
+             {
+               name: "模拟数据",
+               type: "line",
+               showSymbol: false,
+               hoverAnimation: false,
+               data: data
+             }
+           ]
+         };
+
+         setInterval(function () {
+           for (var i = 0; i < 10; i++) {
+             data.shift();
+             data.push(randomData());
+           }
+
+           myChart.setOption({
+             series: [
+               {
+                 data: data
+               }
+             ]
+           });
+         }, 10000);
+         optionBar = {
+           title: {
+           },
+           color: ["#3398DB"],
+           tooltip: {
+             trigger: "axis",
+             axisPointer: {
+               // 坐标轴指示器，坐标轴触发有效
+               type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
+             }
+           },
+           grid: {
+             left: "3%",
+             right: "4%",
+             bottom: "3%",
+             containLabel: true
+           },
+           xAxis: [
+             {
+               type: "category",
+               data: [
+                 "燃烧器故障",
+                 "超压",
+                 "排烟温度高",
+                 "燃气泄漏",
+                 "极限低水位"
+               ],
+               axisTick: {
+                 alignWithLabel: true
+               }
+             }
+           ],
+           yAxis: [
+             {
+               type: "value"
+             }
+           ],
+           series: [
+             {
+               name: "累计次数",
+               type: "bar",
+               barWidth: "60%",
+               data: [5, 3, 7, 6, 9]
+             }
+           ]
+         };
+         if (
+           optionLine &&
+           typeof optionLine === "object" &&
+           (optionBar && typeof optionBar === "object")
+         ) {
+           myChart.setOption(optionLine, true);
+           myChart2.setOption(optionBar, true);
+         }
+       },
+       /**
+        * 地址解析
+        * @param address 地址字符串
+        */
+       getArea (address) {
+         //地址解析
+         let area = {};
+         let index11 = 0;
+         let index1 = address.indexOf("省");
+         if (index1 == -1) {
+           index11 = address.indexOf("自治区");
+           if (index11 != -1) {
+             area.Province = address.substring(0, index11 + 3);
+           } else {
+             area.Province = address.substring(0, 0);
+           }
+         } else {
+           area.Province = address.substring(0, index1 + 1);
+         }
+
+         let index2 = address.indexOf("市");
+         if (index11 == -1) {
+           area.City = address.substring(index11 + 1, index2);
+         } else {
+           if (index11 == 0) {
+             area.City = address.substring(index1 + 1, index2);
+           } else {
+             area.City = address.substring(index11 + 3, index2);
+           }
+         }
+
+         let index3 = address.lastIndexOf("区");
+         if (index3 == -1) {
+           index3 = address.indexOf("县");
+           area.Country = address.substring(index2 + 1, index3 + 1);
+         } else {
+           area.Country = address.substring(index2 + 1, index3 + 1);
+         }
+         return area;
+       },
+       /**
+        * 通过控制器编号获得设备类型和字节数据
+        * @param controllerNo 控制器编号
+        */
+       getLineChartDeviceFocus (controllerNo) {
+         Promise.all([getControllerByteData(this.$store.state.user.deviceRunInfoNo), getControllerType(this.$store.state.user.deviceRunInfoNo)]).then((data) => {
+           let controllerByteData = data[0].data
+           let controllerType = data[1].data.deviceType
+           if (controllerByteData.length > 0 && controllerType) {
+             this.getDeviceByByteDataAndType(controllerByteData, controllerType)
+           }
+         }).catch(function (r) {
+           console.log(r);
+         })
+       },
+       /**
+        * 通过字节数据和设备类型获得设备数据
+        * @param byteData 字节数据
+        * @param deviceType 设备类型
+        */
+       getDeviceByByteDataAndType (byteData, deviceType) {
+         getDeviceByByteDataAndType(byteData, deviceType).then(data => {
+           let focusMap = data.getDeviceFocusFields()
+           console.log(this.chartNameArray)
+           for (let key in focusMap) {
+             if (focusMap[key] != null && focusMap[key].key == "mockInfo") {
+               this.getDeviceFocusMap.title = focusMap[key].title
+               this.getDeviceFocusMap.value = focusMap[key].value
+               this.getDeviceFocusMap.unit = focusMap[key].unit
+               break;
+             }
+           }
+         })
        }
-       new BMapLib.MarkerClusterer(map, {markers: markers});
-      },
-      /**
-       * 绘制报表
-       */
-      drawLine () {
-       //绘制报表
-       var dom = document.getElementById("lineChart");
-       let dom2 = document.getElementById("barChart");
-       var myChart = this.$echarts.init(dom);
-       let myChart2 = this.$echarts.init(dom2);
-       let optionLine = null;
-       let optionBar = null;
-
-       function randomData () {
-        now = new Date(+now + oneDay);
-        value = Math.round(Math.random() * 20 + 120 + 1);
-        return {
-         name: now.toString(),
-         value: [
-          [now.getTime()],
-          Math.round(value)
-         ]
-        };
-       }
-
-       var data = [];
-       var now = new Date(new Date().getTime() - 2 * 60 * 1000)
-       var oneDay = 1 * 1000;
-       var value = Math.random() * 98;
-       for (var i = 0; i < 100; i++) {
-        data.push(randomData());
-       }
-
-       optionLine = {
-        title: {
-         text: "出口温度实时曲线"
-        },
-        tooltip: {
-         trigger: "axis",
-         formatter: function (params) {
-          params = params[0];
-          var date = new Date(params.name);
-          return (
-           date.getMinutes() +
-           " : " +
-           params.value[1]
-          );
-         },
-         axisPointer: {
-          animation: false
-         }
-        },
-        xAxis: {
-         type: "time",
-         splitLine: {
-          show: false
-         }
-        },
-        yAxis: {
-         type: "value",
-         boundaryGap: [0, "100%"],
-         splitLine: {
-          show: false
-         }
-        },
-        series: [
-         {
-          name: "模拟数据",
-          type: "line",
-          showSymbol: false,
-          hoverAnimation: false,
-          data: data
-         }
-        ]
-       };
-
-       setInterval(function () {
-        for (var i = 0; i < 10; i++) {
-         data.shift();
-         data.push(randomData());
-        }
-
-        myChart.setOption({
-         series: [
-          {
-           data: data
-          }
-         ]
-        });
-       }, 10000);
-       optionBar = {
-        title: {
-         text: "报警统计"
-        },
-        color: ["#3398DB"],
-        tooltip: {
-         trigger: "axis",
-         axisPointer: {
-          // 坐标轴指示器，坐标轴触发有效
-          type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
-         }
-        },
-        grid: {
-         left: "3%",
-         right: "4%",
-         bottom: "3%",
-         containLabel: true
-        },
-        xAxis: [
-         {
-          type: "category",
-          data: [
-           "燃烧器故障",
-           "超压",
-           "排烟温度高",
-           "燃气泄漏",
-           "极限低水位"
-          ],
-          axisTick: {
-           alignWithLabel: true
-          }
-         }
-        ],
-        yAxis: [
-         {
-          type: "value"
-         }
-        ],
-        series: [
-         {
-          name: "累计次数",
-          type: "bar",
-          barWidth: "60%",
-          data: [5, 3, 7, 6, 9]
-         }
-        ]
-       };
-       if (
-        optionLine &&
-        typeof optionLine === "object" &&
-        (optionBar && typeof optionBar === "object")
-       ) {
-        myChart.setOption(optionLine, true);
-        myChart2.setOption(optionBar, true);
-       }
-      },
-      /**
-       * 地址解析
-       * @param address 地址字符串
-       */
-      getArea (address) {
-       //地址解析
-       let area = {};
-       let index11 = 0;
-       let index1 = address.indexOf("省");
-       if (index1 == -1) {
-        index11 = address.indexOf("自治区");
-        if (index11 != -1) {
-         area.Province = address.substring(0, index11 + 3);
-        } else {
-         area.Province = address.substring(0, 0);
-        }
-       } else {
-        area.Province = address.substring(0, index1 + 1);
-       }
-
-       let index2 = address.indexOf("市");
-       if (index11 == -1) {
-        area.City = address.substring(index11 + 1, index2);
-       } else {
-        if (index11 == 0) {
-         area.City = address.substring(index1 + 1, index2);
-        } else {
-         area.City = address.substring(index11 + 3, index2);
-        }
-       }
-
-       let index3 = address.lastIndexOf("区");
-       if (index3 == -1) {
-        index3 = address.indexOf("县");
-        area.Country = address.substring(index2 + 1, index3 + 1);
-       } else {
-        area.Country = address.substring(index2 + 1, index3 + 1);
-       }
-       return area;
-      },
-
      }
     };
 </script>
