1df394a238622b057c031fe1b27a8cb518fb7dbc
jialiang.li <80201288@qq.com>
2019-07-12 11:15:30 +0800

    调整图片上传功能

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,14 +1,8 @@
 import request from '@/utils/request'
 
-export function uploadFile () {
-  return request({
-    url: '/webapi/boilermanage/upload/uploadFile',
-    method: 'post'
-  })
-}
 export function deleteFile (path) {
   return request({
-    url: '/webapi/boilermanage/upload/deleteFile',
+    url: '/webapi/boilermanage/file/deleteFile',
     method: 'post',
     params: {path}
   })
