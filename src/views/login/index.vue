98b58db49e9293180d8e449d6040229c9352215f
zl1215422787 <48305059+zl1215422787@users.noreply.github.com>
2019-08-30 10:32:07 +0800

    微信登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,183 +1,226 @@
 <template>
-  <div class="loginBox" :style="{backgroundImage: 'url(' + backgroundUrl + ')'}">
-    <el-form
-      :model="loginForm"
-      :rules="rules"
-      ref="loginForm"
-      label-position="left"
-      label-width="0px"
-      class="login-container"
+  <div v-if="wechatlogin">
+    <login-dialog
+      :backgroundUrl="backgroundUrl"
+      :logoUrl="logoUrl"
+      :image="image"
+      :copyrightInfo1="copyrightInfo1"
+      :copyrightInfo2="copyrightInfo2"
+      @handleLogin="handleLogin"
+      @back="back"
     >
-      <div class="picBox">
-        <img class="pic" :src="logoUrl" v-if="logoUrl!=''" />
-      </div>
-      <h3 class="title">锅炉远程监控平台</h3>
-      <el-form-item prop="account">
-        <el-input type="text" v-model="loginForm.account" auto-complete="off" placeholder="账号"></el-input>
-      </el-form-item>
-      <el-form-item prop="passWord">
-        <el-input type="password" v-model="loginForm.passWord" auto-complete="off" placeholder="密码"></el-input>
-      </el-form-item>
-      <!--      &lt;!&ndash;<el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>&ndash;&gt;-->
-      <el-form-item style="width:100%;">
-        <el-button
-          type="primary"
-          style="width:100%;"
-          @click.native.prevent="handleLogin"
-          :loading="loading"
-        >登录</el-button>
-      </el-form-item>
-      <p class="copyright" v-if="copyrightInfo1!=''">{{copyrightInfo1}} © 2000-2019</p>
-      <p class="copyright" v-if="copyrightInfo2!=''">{{copyrightInfo2}} © 2000-2019</p>
-    </el-form>
+    </login-dialog>
   </div>
 </template>
 <script>
-import { login, getUserInfo, a, b, c } from "@/api/login";
-import { resolve } from "url";
+  import { login, getUserInfo,wechatLogin, a, b, c } from "@/api/login";
+  import loginDialog from "@sdcsoft/components/components/login/index";
 
-export default {
-  name: "login",
-  data() {
-    return {
-      loading: false,
-      copyrightInfo1: "",
-      copyrightInfo2: "",
-      logoUrl: "/static/common/defaultLogo.png",
-      backgroundUrl: "/static/common/loginBackground.jpg",
-
-      loading: false,
-
-      loginForm: {
-        account: "",
-        passWord: ""
-      },
-      rules: {
-        account: [{ required: true, message: "请输入账号", trigger: "blur" }],
-        passWord: [{ required: true, message: "请输入密码", trigger: "blur" }]
-      }
-    };
-  },
-  created() {
-    this.initCopyrightInfoAndLogoUrl();
-  },
-
-  methods: {
-    handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        let org = 0
-        if (valid) {
-          this.loading = true;
-          let baseInfo,resources;
-          login(this.loginForm.account.trim(), this.loginForm.passWord)
-            .then(response => {
-              let data = response.data;
-              baseInfo = data.data;
-              if (data.code == 0) {
-                org = baseInfo.orgId
-                return getUserInfo(baseInfo.id);
-              } else {
-                return Promise.reject(data.msg);
-              }
-            })
-            .then(response => {
-              let data = response.data
-              if (data.code == 0) {
-                return this.$store.dispatch("saveUserState", {
-                  "baseInfo": baseInfo,
-                  "sysInfo": data.data,
-                  "router": this.$router
-                });
-              }
-            })
-            .then(() => {
-              //记录orgid
-               window.localStorage["logoUrl"] = '/files/'+org+'/logo.jpg'
-              this.loading = false
-              this.$router.push({ path: this.redirect || "/index" });
-            })
-            .catch(reason => {
-              this.loading = false
-              this.$message.error(reason);
-            });
-        } else {
-          return false;
-        }
-      });
+  export default {
+    name: "login",
+    components: {
+      loginDialog
     },
-    initCopyrightInfoAndLogoUrl() {
-      let copyrightInfo1 = window.localStorage["copyrightInfo1"];
-      let copyrightInfo2 = window.localStorage["copyrightInfo2"];
-      if (copyrightInfo1) {
-        this.copyrightInfo1 = window.localStorage["copyrightInfo1"];
-      }
-      if (copyrightInfo2) {
-        this.copyrightInfo2 = window.localStorage["copyrightInfo2"];
-      }
+    data() {
+      return {
+        image: false,
+        wechatlogin: true,
+        loading: false,
+        copyrightInfo1: "",
+        copyrightInfo2: "",
+        logoUrl: "/static/common/defaultLogo.png",
+        backgroundUrl: "/static/common/loginBackground.jpg",
 
-      let logoUrl = window.localStorage["logoUrl"];
-      let bgUrl = window.localStorage["bgUrl"];
-      if (logoUrl) {
-        this.logoUrl = window.localStorage["logoUrl"];
+        loading: false,
+
+        loginForm: {
+          account: null,
+          passWord: null
+        },
+        rules: {
+          account: [{ required: true, message: "请输入账号", trigger: "blur" }],
+          passWord: [{ required: true, message: "请输入密码", trigger: "blur" }]
+        }
+      };
+    },
+    created() {
+      this.loginForm.account = this.getUrlKey('mobile')
+      this.loginForm.passWord = this.getUrlKey('token')
+      if(this.loginForm.account!=null){
+        if(this.loginForm.account == 0){
+          this.image=true
+        } else {
+          this.wechatlogin=false;
+          this.wechatLogin()
+        }
+      }else{
+        this.initCopyrightInfoAndLogoUrl();
       }
-      if (bgUrl) {
-        this.backgroundUrl = window.localStorage["bgUrl"];
+      /*  if(this.employeeId!=0){
+          this.wechatlogin=false;
+          if(this.employeeId==0){
+            this.image=true
+          } else {
+        } else {
+          this.initCopyrightInfoAndLogoUrl();
+        }*/
+    },
+
+    methods: {
+      getUrlKey (name) {
+        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null
+      },
+      back(){
+        this.image=false;
+      },
+      wechatLogin(){
+        let org = 0
+        this.loading = true;
+        let baseInfo,resources;
+        login(this.loginForm.account.trim(), this.loginForm.passWord)
+          .then(response => {
+            let data = response.data;
+            baseInfo = data.data;
+            console.log(baseInfo);
+            if (data.code == 0) {
+              org = baseInfo.orgId
+              return getUserInfo(baseInfo.id);
+            } else {
+              return Promise.reject(data.msg);
+            }
+          })
+          .then(response => {
+            let data = response.data
+            if (data.code == 0) {
+              return this.$store.dispatch("saveUserState", {
+                "baseInfo": baseInfo,
+                "sysInfo": data.data,
+                "router": this.$router
+              });
+            }
+          })
+          .then(() => {
+            //记录orgid
+            window.localStorage["logoUrl"] = '/files/' + org + '/logo.jpg'
+            this.loading = false
+            this.$router.push({ path: this.redirect || "/index" });
+          })
+          .catch(reason => {
+            this.loading = false
+            this.$message.error(reason);
+          });
+      },
+      handleLogin(loginForm) {
+        let org = 0
+        this.loading = true;
+        let baseInfo,resources;
+        login(loginForm.account.trim(), loginForm.passWord)
+          .then(response => {
+            let data = response.data;
+            baseInfo = data.data;
+            console.log(baseInfo);
+            if (data.code == 0) {
+              org = baseInfo.orgId
+              return getUserInfo(baseInfo.id);
+            } else {
+              return Promise.reject(data.msg);
+            }
+          })
+          .then(response => {
+            let data = response.data
+            if (data.code == 0) {
+              return this.$store.dispatch("saveUserState", {
+                "baseInfo": baseInfo,
+                "sysInfo": data.data,
+                "router": this.$router
+              });
+            }
+          })
+          .then(() => {
+            //记录orgid
+            window.localStorage["logoUrl"] = '/files/' + org + '/logo.jpg'
+            this.loading = false
+            this.$router.push({ path: this.redirect || "/index" });
+          })
+          .catch(reason => {
+            this.loading = false
+            this.$message.error(reason);
+          });
+      },
+      initCopyrightInfoAndLogoUrl() {
+        let copyrightInfo1 = window.localStorage["copyrightInfo1"];
+        let copyrightInfo2 = window.localStorage["copyrightInfo2"];
+        if (copyrightInfo1) {
+          this.copyrightInfo1 = window.localStorage["copyrightInfo1"];
+        }
+        if (copyrightInfo2) {
+          this.copyrightInfo2 = window.localStorage["copyrightInfo2"];
+        }
+
+        let logoUrl = window.localStorage["logoUrl"];
+        let bgUrl = window.localStorage["bgUrl"];
+        if (logoUrl) {
+          this.logoUrl = window.localStorage["logoUrl"];
+        }
+        if (bgUrl) {
+          this.backgroundUrl = window.localStorage["bgUrl"];
+        }
       }
     }
-  }
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-.login-container {
-  width: 350px;
-  margin: 100px auto auto auto;
-  padding: 35px 35px 15px 35px;
-  -webkit-border-radius: 5px;
-  border-radius: 5px;
-  -moz-border-radius: 5px;
-  background-clip: padding-box;
-  background: #fff;
-  border: 1px solid #eaeaea;
-  box-shadow: 0 0 25px #cac6c6;
-  .title {
-    margin: 20px auto 20px auto;
+  .login-container {
+    width: 350px;
+    height: 400px;
+    margin: 100px auto auto auto;
+    padding: 35px 35px 15px 35px;
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+    .title {
+      margin: 20px auto 20px auto;
+      text-align: center;
+      color: #505458;
+    }
+    .remember {
+      margin: 0px 0px 35px 0px;
+    }
+  }
+  .loginBox {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    /*background-image: url("../../../static/common/loginBackground.jpg");*/
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+  }
+  .copyright {
+    margin: 0px auto;
     text-align: center;
     color: #505458;
+    font-size: small;
   }
-  .remember {
-    margin: 0px 0px 35px 0px;
+  .picBox {
+    text-align: center;
   }
-}
-.loginBox {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  /*background-image: url("../../../static/common/loginBackground.jpg");*/
-  background-position: center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-size: cover;
-}
-.copyright {
-  margin: 0px auto;
-  text-align: center;
-  color: #505458;
-  font-size: small;
-}
-.picBox {
-  text-align: center;
-}
-.pic {
-  margin: 15px;
-  width: auto;
-  height: 100px;
-  -webkit-border-radius: 5px;
-  border-radius: 5px;
-  -moz-border-radius: 5px;
-  background-clip: padding-box;
-  background: #fff;
-  border: 1px solid #eaeaea;
-  box-shadow: 0 0 25px #cac6c6;
-}
+  .pic {
+    margin: 15px;
+    width: auto;
+    height: 100px;
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
 </style>
